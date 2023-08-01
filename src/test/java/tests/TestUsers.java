83450bacc5605a62fcc3183d93ef4d0bb8b24955
Wavda Aniva <wavdanv@gmail.com>
2023-08-01 11:32:36 +0700

    Update Assertion

--- src/test/java/tests/TestUsers.java
+++ src/test/java/tests/TestUsers.java
@@ -13,28 +13,34 @@ import static org.testng.Assert.assertTrue;
 public class TestUsers {
     private JSONObject listUsers;
     private JSONObject response;
-    private UsersRequest usersRequest;
+    private UsersRequest users;
 
     @BeforeMethod
     void before() {
-        usersRequest = new UsersRequest();
+        users = new UsersRequest();
     }
 
     @Test(description = "GET list users")
     void getListUsers() {
-        response = listUsers = usersRequest.getListUsers(null, null);
-        this.assertGetList(1, 6, 6);
+        response = listUsers = users.getListUsers(null, null);
+
+        // Assertions
+        users.verifyStatusCode(200);
+        this.doAssertGetList(1, 6);
     }
 
     @Test(description = "GET list users with custom 'page' and 'per_page'")
     void getListUsersWithCustomPageAndPerPage() {
-        response = usersRequest.getListUsers(2, 4);
-        this.assertGetList(2, 4, 4);
+        response = users.getListUsers(2, 4);
+
+        // Assertions
+        users.verifyStatusCode(200);
+        this.doAssertGetList(2, 4);
     }
 
     @Test(description = "GET detail user with valid User ID", dependsOnMethods = "getListUsers")
     void getDetailUserWithValidUserId() {
-        response = usersRequest.getDetailUser("4");
+        response = users.getDetailUser("4");
 
         // Get expected values from the listUsers response
         JsonPath list = JsonPath.from(listUsers.toString());
@@ -44,12 +50,40 @@ public class TestUsers {
         String avatar = list.get("data.find { it.id == 4 }.avatar");
 
         // Assertions
-        usersRequest.verifyStatusCode(200);
+        users.verifyStatusCode(200);
         JSONObject dataObject = response.getJSONObject("data");
         assertEquals(dataObject.get("email"), email);
         assertEquals(dataObject.get("first_name"), firstName);
         assertEquals(dataObject.get("last_name"), lastName);
         assertEquals(dataObject.get("avatar"), avatar);
+    }
+
+    @Test(description = "GET detail user with non-existent User ID")
+    void getDetailUserWithNonExistentUserId() {
+        int nonExistentUserId = listUsers.getInt("total") + 1;
+        response = users.getDetailUser(String.valueOf(nonExistentUserId));
+
+        // Assertions
+        users.verifyStatusCode(404);
+        assertTrue(response.isEmpty());
+    }
+
+    @Test(description = "GET detail user with invalid numeric value as User ID")
+    void getDetailUserWithInvalidNumericValueAsUserId() {
+        response = users.getDetailUser("-1");
+
+        // Assertions
+        users.verifyStatusCode(404);
+        assertTrue(response.isEmpty());
+    }
+
+    @Test(description = "GET detail user with non-numeric value as User ID")
+    void getDetailUserWithNonNumericValueAsUserId() {
+        response = users.getDetailUser("invalid");
+
+        // Assertions
+        users.verifyStatusCode(404);
+        assertTrue(response.isEmpty());
     }
 
     @DataProvider(name = "InvalidUserId")
@@ -63,25 +97,19 @@ public class TestUsers {
 
     @Test(dataProvider = "InvalidUserId", description = "GET detail user with invalid User ID")
     public void getDetailUserWithInvalidUserId(String userId) {
-        response = usersRequest.getDetailUser(userId);
-        usersRequest.verifyStatusCode(404);
+        response = users.getDetailUser(userId);
+
+        // Assertions
+        users.verifyStatusCode(404);
         assertTrue(response.isEmpty());
     }
 
-    /**
-     * Asserts the response of a GET list request by verifying the status code, page number, per page count,
-     * total count, total pages, and length of the data array.
-     *
-     * @param page        The expected page number.
-     * @param perPage     The expected number of items per page.
-     * @param dataLength  The expected length of the data array in the response.
-     */
-    private void assertGetList(int page, int perPage, int dataLength) {
-        usersRequest.verifyStatusCode(200);
+    private void doAssertGetList(int page, int perPage) {
+        users.verifyStatusCode(200);
         assertEquals(response.getInt("page"), page);
         assertEquals(response.getInt("per_page"), perPage);
         assertTrue(response.getInt("total") > 0);
-        assertEquals(response.getInt("total_pages"), response.getInt("total") / response.getInt("per_page"));
-        assertEquals(response.getJSONArray("data").length(), dataLength);
+        assertEquals(response.getInt("total_pages"), (int) Math.ceil(response.getInt("total") / response.getInt("per_page")));
+        assertEquals(response.getJSONArray("data").length(), perPage);
     }
 }
